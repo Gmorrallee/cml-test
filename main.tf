9c81c51194b014fb20e27f5fce7e23e0d6b73654
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 11:23:50 +0000

    Update vnet module version to 0.17

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "rg" {
 
 module "vnet" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
-  version = "~> 0.7"
+  version = "~> 0.17"
 
   name                = "vnet-identity"
   resource_group_name = module.rg.name
