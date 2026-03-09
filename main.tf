5585993a0da29bbfa9280082d21c7751554d1e4e
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 13:30:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,5 +17,13 @@ module "vnet" {
   parent_id  = azurerm_resource_group.rg_identity_networking.id
   location   = var.location
 
-  address_space = ["10.100.0.0/16"]
+  address_space = ["10.100.0.0/16"],
+
+  name        = "vnet-identity-uks"
+  parent_id  = azurerm_resource_group.rg_identity_networking.id
+  location   = var.location_2
+
+  address_space = ["10.200.0.0/16"]
 }
+
+
