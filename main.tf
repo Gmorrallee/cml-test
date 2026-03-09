8783acd18fb6119a8959a251ffb6f3989d6894f5
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 12:48:01 +0000

    Rename resource group and update VNet configuration

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "identity" {
-  name     = "rg-identity-prod"
+resource "azurerm_resource_group" "rg-identity-networking" {
+  name     = "rg-identity-networking"
   location = var.location
   providers = {
     azurerm = var.identity_subscription_id
@@ -11,10 +11,10 @@ module "vnet" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "~> 0.17"
 
-  name       = "vnet-identity"
-  parent_id = azurerm_resource_group.identity.id
+  name       = "vnet-identity-uks"
+  parent_id = azurerm_resource_group.rg-identity-networking.id
   location  = var.location
 
-  address_space = ["10.10.0.0/16"]
+  address_space = ["10.100.0.0/16"]
 }
 
