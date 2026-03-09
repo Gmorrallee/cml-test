9b4a5ce4f01872685c980bc7ec9a750d44b91923
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 13:32:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg_identity_networking" {
   location = var.location
 }
 
-module "vnet" {
+module "vnet_identity_uksouth" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "~> 0.17"
 
@@ -16,13 +16,20 @@ module "vnet" {
   name        = "vnet-identity-uks"
   parent_id  = azurerm_resource_group.rg_identity_networking.id
   location   = var.location
+  address_space = ["10.100.0.0/16"]
+}
 
-  address_space = ["10.100.0.0/16"],
+module "vnet_identity_ukwest" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.17"
 
-  name        = "vnet-identity-uks"
+  providers = {
+    azurerm = azurerm.identity
+  }
+
+  name        = "vnet-identity-ukw"
   parent_id  = azurerm_resource_group.rg_identity_networking.id
   location   = var.location_2
-
   address_space = ["10.200.0.0/16"]
 }
 
