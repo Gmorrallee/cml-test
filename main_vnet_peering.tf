2a79fae550b1b081b4fd9141533271631a0cf1c0
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 18:07:20 +0000

    Update main_vnet_peering.tf

--- main_vnet_peering.tf
+++ main_vnet_peering.tf
@@ -15,6 +15,23 @@ module "identity_vnet_peering" {
   reverse_name           = "vnet-identity-ukwest-to-vnet-identity-uksouth"
 }
 
+module "identity_to_connectivity_vnet_peering" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/peering"
+  version = "~> 0.17"
+
+  name                      = "vnet-identity-uksouth-to-vnet-connectivity-uksouth"
+  parent_id                 = module.vnet_identity_uksouth.resource_id
+  remote_virtual_network_id = module.vnet_connectivity_uksouth.resource_id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+
+  create_reverse_peering = true
+  reverse_name           = "vnet-connectivity-uksouth-to-vnet-identity-uksouth"
+}
+
 module "management_vnet_peering" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/peering"
   version = "~> 0.17"
