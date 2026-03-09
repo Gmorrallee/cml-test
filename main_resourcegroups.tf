882354342b723453014885c5083592390d20e0a3
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 16:01:18 +0000

    Update main_resourcegroups.tf

--- main_resourcegroups.tf
+++ main_resourcegroups.tf
@@ -1,48 +1,37 @@
+
 resource "azurerm_resource_group" "rg_management_management" {
   provider = azurerm.management
-
-  name     = "rg_management_management"
+  name     = "rg-management-management"
   location = var.location
 }
 
 resource "azurerm_resource_group" "rg_management_networking" {
   provider = azurerm.management
-
-  name     = "rg_management_networking"
+  name     = "rg-management-networking"
   location = var.location
 }
 
 resource "azurerm_resource_group" "rg_identity_networking" {
   provider = azurerm.identity
-
   name     = "rg-identity-networking"
   location = var.location
 }
 
 resource "azurerm_resource_group" "rg_identity_management" {
   provider = azurerm.identity
-
-  name     = "rg-identity-networking"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_identity_networking" {
-  provider = azurerm.identity
-
-  name     = "rg-identity-networking"
+  name     = "rg-identity-management"
   location = var.location
 }
 
 resource "azurerm_resource_group" "rg_identity_dirservices" {
-  provider = azurerm.connectivity
-
-  name     = "rg-identity-networking"
+  provider = azurerm.identity
+  name     = "rg-identity-dirservices"
   location = var.location
 }
 
 resource "azurerm_resource_group" "rg_connectivity_management" {
   provider = azurerm.connectivity
-
-  name     = "rg_conectivity_management"
+  name     = "rg-connectivity-management"
   location = var.location
 }
+
