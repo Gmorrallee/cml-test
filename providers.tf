77b96ea737a1da27fa4c0e8379309310ca0fdbda
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 12:11:32 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,3 +2,26 @@ provider "azurerm" {
   features {}
   use_oidc = true
 }
+
+
+provider "azurerm" {
+  alias           = "management"
+  features        {}
+  use_oidc        = true
+  subscription_id = var.subscriptions.managment
+}
+
+provider "azurerm" {
+  alias           = "connectivity"
+  features        {}
+  use_oidc        = true
+  subscription_id = var.subscriptions.connectivity
+}
+
+provider "azurerm" {
+  alias           = "identity"
+  features        {}
+  use_oidc        = true
+  subscription_id = var.subscriptions.identity
+}
+
