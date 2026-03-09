21a75fea46e587900ffe19804b6ade57ad6790fe
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 16:36:52 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,28 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      # version = "~> 4.0"   # optional pin
-    }
-    azapi = {
-      source = "Azure/azapi"
-      # version = "~> 2.0"   # matches AVM peering requirement
-    }
-  }
-}
-
-variable "subscriptions" {
-  type = map(string)
-}
-
-# Identity subscription (default)
 provider "azurerm" {
   features {}
   use_oidc        = true
   subscription_id = var.subscriptions["identity"]
 }
 
-# Management subscription
 provider "azurerm" {
   alias           = "management"
   features        {}
@@ -30,7 +11,6 @@ provider "azurerm" {
   subscription_id = var.subscriptions["management"]
 }
 
-# Connectivity subscription
 provider "azurerm" {
   alias           = "connectivity"
   features        {}
@@ -38,8 +18,8 @@ provider "azurerm" {
   subscription_id = var.subscriptions["connectivity"]
 }
 
-# AzAPI (used by AVM peering submodule)
 provider "azapi" {
   use_oidc        = true
   subscription_id = var.subscriptions["identity"]
 }
+``
