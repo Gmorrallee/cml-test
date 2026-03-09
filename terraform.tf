98840284fbd38f6d518098e3ed45d781aed5a055
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 10:40:09 +0000

    Configure Terraform backend for Azure

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,18 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "satfstateprod"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
 
+    use_oidc  = true
+    client_id = "6cfec517-1dcd-4183-98a3-3f60eeafdbd7"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
