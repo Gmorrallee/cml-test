bbf206fbee57a58ed25a20e7e59aeace61b3e8a6
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 12:59:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
-resource "azurerm_resource_group" "rg-identity-networking" {
+resource "azurerm_resource_group" "rg_identity_networking" {
+  provider = azurerm.identity
+
   name     = "rg-identity-networking"
   location = var.location
-  providers = {
-    azurerm = var.identity_subscription_id
-  }
-
 }
 
 module "vnet" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "~> 0.17"
 
-  name       = "vnet-identity-uks"
-  parent_id = azurerm_resource_group.rg-identity-networking.id
-  location  = var.location
+  providers = {
+    azurerm = azurerm.identity
+  }
+
+  name        = "vnet-identity-uks"
+  parent_id  = azurerm_resource_group.rg_identity_networking.id
+  location   = var.location
 
   address_space = ["10.100.0.0/16"]
 }
-
