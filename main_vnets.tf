c390adc30bbeacf947268c03ce41bfee1b1944ce
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 16:26:45 +0000

    Update main_vnets.tf

--- main_vnets.tf
+++ main_vnets.tf
@@ -39,12 +39,8 @@ module "identity_vnet_peering" {
   parent_id                 = module.vnet_identity_uksouth.resource_id
   remote_virtual_network_id = module.vnet_identity_ukwest.resource_id
 
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  allow_gateway_transit        = false
-  use_remote_gateways          = false
-
   create_reverse_peering = true
   reverse_name           = "vnet-identity-ukwest-to-vnet-identity-uksouth"
 }
+
 
