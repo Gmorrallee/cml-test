f1cef7b7fac98fc517139762a4110efc965b4ed7
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 17:32:33 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,6 +11,14 @@ terraform {
   }
 }
 
+
+provider "azurerm" {
+  alias           = "identity"
+  features        {}
+  use_oidc        = true
+  subscription_id = var.subscriptions["identity"]
+
+
 provider "azurerm" {
   features {}
   use_oidc        = true
