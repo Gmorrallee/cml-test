361a65768a87711abdb4205ca531401d750e629c
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 17:46:41 +0000

    Update main_vnets.tf

--- disabled/main_vnets.tf
+++ disabled/main_vnets.tf
@@ -1,3 +1,4 @@
+## Identity vNet 
 
 module "vnet_identity_uksouth" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
@@ -25,6 +26,66 @@ module "vnet_identity_ukwest" {
   parent_id     = azurerm_resource_group.rg_identity_networking.id
   location      = var.location_2
   address_space = ["10.200.0.0/16"]
+}
+
+## Management vNet 
+
+module "vnet_management_uksouth" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.17"
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  name          = "vnet-management-uks"
+  parent_id     = azurerm_resource_group.rg_menagement_networking.id
+  location      = var.location
+  address_space = ["10.101.0.0/16"]
+}
+
+module "vnet_management_ukwest" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.17"
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  name          = "vnet-management-ukw"
+  parent_id     = azurerm_resource_group.rg_management_networking.id
+  location      = var.location_2
+  address_space = ["10.201.0.0/16"]
+}
+
+## Connectivity vNet 
+
+module "vnet_connectivity_uksouth" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.17"
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  name          = "vnet-connectivity-uks"
+  parent_id     = azurerm_resource_group.rg_connectivity_networking.id
+  location      = var.location
+  address_space = ["10.102.0.0/16"]
+}
+
+module "vnet_connectivity_ukwest" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.17"
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  name          = "vnet-connectivity-ukw"
+  parent_id     = azurerm_resource_group.rg_connectivity_networking.id
+  location      = var.location_2
+  address_space = ["10.202.0.0/16"]
 }
 
 
