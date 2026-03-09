983c4529444add57315731850b50f76512b93b98
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 11:43:29 +0000

    Add random provider to Terraform configuration

--- terraform.tf
+++ terraform.tf
@@ -5,14 +5,15 @@ terraform {
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
 
-    use_oidc  = true
-    client_id = "6cfec517-1dcd-4183-98a3-3f60eeafdbd7"
+    use_oidc = true
   }
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-#      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 }
