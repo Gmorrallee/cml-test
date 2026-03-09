462a2eff80295961372836b82357dfcabf40f1d1
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-09 11:07:28 +0000

    Add resource group and virtual network modules

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+module "rg" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "~> 0.4"
 
+  name     = "rg-identity-prod"
+  location = var.location
+}
+
+module "vnet" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 0.7"
+
+  name                = "vnet-identity"
+  resource_group_name = module.rg.name
+  location            = var.location
+  address_space       = ["10.10.0.0/16"]
+}
