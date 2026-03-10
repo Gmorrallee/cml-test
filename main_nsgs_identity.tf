8ae48aef6f92a50661b46422fb566edf59097835
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 13:01:50 +0000

    Update main_nsgs_identity.tf

--- main_nsgs_identity.tf
+++ main_nsgs_identity.tf
@@ -1,9 +1,10 @@
 module "nsg-dirservices-uks" {
-  source  = "Azure/avm-res-network-networksecuritygroup/azurerm//examples/default"
+  source  = "Azure/avm-res-network-networksecuritygroup/azurerm"
   version = "0.5.1"
 
-  location = var.location
-  name = "nsg-dirservices-uks"
-  parent_id     = azurerm_resource_group.rg_identity_networking.id
-  }
+  location            = var.location
+  name                = "nsg-dirservices-uks"
+  resource_group_name = azurerm_resource_group.rg_identity_networking.name
+}
+
   
