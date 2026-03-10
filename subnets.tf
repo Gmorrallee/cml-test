c166b31cfa6f798b4fd01e890bd320dfa5e96b50
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 11:28:39 +0000

    Create subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,19 @@
+module "snet_dirservices-uks" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_identity.resource_id
+  name      = "snet-dirservices-uks"
+
+  address_prefixes = ["10.100.10.0/24"]
+}
+
+module "snet_dirservices-ukw" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_identity.resource_id
+  name      = "snet-dirservices-ukw"
+
+  address_prefixes = ["10.200.10.0/24"]
+}
