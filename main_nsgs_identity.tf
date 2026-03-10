e7806c67241cf0bf54acfea9211dc5fab50d3563
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 13:05:42 +0000

    Update main_nsgs_identity.tf

--- main_nsgs_identity.tf
+++ main_nsgs_identity.tf
@@ -7,4 +7,25 @@ module "nsg-dirservices-uks" {
   resource_group_name = azurerm_resource_group.rg_identity_networking.name
 }
 
+resource "azurerm_subnet_network_security_group_association" "nsg-dirservices-uks" {
+  subnet_id                 = data.azurerm_subnet.snet-dirservices-uks.id
+  network_security_group_id = module.nsg-dirservices-uks.resource_id
+}
+
+
+module "nsg-dirservices-ukw" {
+  source  = "Azure/avm-res-network-networksecuritygroup/azurerm"
+  version = "0.5.1"
+
+  location            = var.location
+  name                = "nsg-dirservices-ukw"
+  resource_group_name = azurerm_resource_group.rg_identity_networking.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-dirservices-ukw" {
+  subnet_id                 = data.azurerm_subnet.snet-dirservices-ukw.id
+  network_security_group_id = module.nsg-dirservices-uks.resource_id
+}
+
+
   
