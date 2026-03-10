bbd18a64eb44054aaccbb441637b6185ddafaabf
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 13:25:47 +0000

    Update main_nsgs_identity.tf

--- main_nsgs_identity.tf
+++ main_nsgs_identity.tf
@@ -1,3 +1,30 @@
+locals {
+  nsg_rules = {
+    "Bastion-Allow" = {
+      name                       = "Bastion-Allow"
+      access                     = "Allow"
+      destination_address_prefix = "*"
+      destination_port_range     = "22,3389"
+      direction                  = "Inbound"
+      priority                   = 100
+      protocol                   = "Tcp"
+      source_address_prefix      = "10.102.250.0/24,10.202.250.0/24"
+      source_port_range          = "*"
+    }
+    "rule02" = {
+      name                       = "Deny-Any-All"
+      access                     = "Deny"
+      destination_address_prefix = "*"
+      destination_port_ranges    = [*]
+      direction                  = "Inbound"
+      priority                   = 4000
+      protocol                   = "*"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+    }
+  }
+}
+
 module "nsg-dirservices-uks" {
   source  = "Azure/avm-res-network-networksecuritygroup/azurerm"
   version = "0.5.1"
@@ -5,6 +32,7 @@ module "nsg-dirservices-uks" {
   location            = var.location
   name                = "nsg-dirservices-uks"
   resource_group_name = azurerm_resource_group.rg_identity_networking.name
+  security_rules      = local.nsg_rules
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-dirservices-uks" {
@@ -20,6 +48,7 @@ module "nsg-dirservices-ukw" {
   location            = var.location_2
   name                = "nsg-dirservices-ukw"
   resource_group_name = azurerm_resource_group.rg_identity_networking.name
+  security_rules      = local.nsg_rules
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-dirservices-ukw" {
