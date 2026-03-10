6f8cbfc4e72516781d1327e3f240e01d405d5369
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 19:29:38 +0000

    Update subnets_identity.tf

--- subnets_identity.tf
+++ subnets_identity.tf
@@ -17,23 +17,3 @@ module "snet-dirservices-ukw" {
 
   address_prefixes = ["10.200.10.0/24"]
 }
-
-module "snet-bastion-uks" {
-  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
-  version = "~> 0.17"
-
-  parent_id = module.vnet_connectivity_uksouth.resource_id
-  name      = "AzureBastionSubnet"
-
-  address_prefixes = ["10.102.250.0/24"]
-}
-
-module "snet-bastion-ukW" {
-  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
-  version = "~> 0.17"
-
-  parent_id = module.vnet_connectivity_ukwest.resource_id
-  name      = "AzureBastionSubnet"
-
-  address_prefixes = ["10.202.250.0/24"]
-}
