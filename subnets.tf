42c771c5bd43be1f33e1fd6f38623d4b1b6b0cc1
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-10 11:32:44 +0000

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -2,7 +2,7 @@ module "snet_dirservices-uks" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
   version = "~> 0.17"
 
-  parent_id = module.vnet_identity.resource_id
+  parent_id = module.vnet_identity_uksouth.resource_id
   name      = "snet-dirservices-uks"
 
   address_prefixes = ["10.100.10.0/24"]
@@ -12,7 +12,7 @@ module "snet_dirservices-ukw" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
   version = "~> 0.17"
 
-  parent_id = module.vnet_identity.resource_id
+  parent_id = module.vnet_identity_ukwest.resource_id
   name      = "snet-dirservices-ukw"
 
   address_prefixes = ["10.200.10.0/24"]
