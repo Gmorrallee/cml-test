32e0a92ef01bab50540653346668d960d0f0df37
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 15:47:06 +0000

    Update dc_vm.tf

--- dc_vm.tf
+++ dc_vm.tf
@@ -1,3 +1,16 @@
+############################
+# Generate admin password
+############################
+
+resource "random_password" "vm_admin" {
+  length  = 20
+  special = true
+}
+
+############################
+# Domain Controller VM (UKS)
+############################
+
 module "vm_dc_uks" {
   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
   version = "~> 0.20"
@@ -7,24 +20,15 @@ module "vm_dc_uks" {
   resource_group_name = azurerm_resource_group.rg_identity_dirservices.name
 
   os_type = "Windows"
-
-  size = "Standard_D2s_v5"
-
-resource "random_password" "vm_admin" {
-  length  = 20
-  special = true
-
-resource "random_password" "vm_admin" {
-  length  = 20
-  special = true
+  size    = "Standard_D2s_v5"
 
   admin_username = "localadmin"
-  admin_password = admin_password = random_password.vm_admin.result
+  admin_password = random_password.vm_admin.result
 
   network_interfaces = {
     primary = {
-      name      = "nic-vm-mgmt-uks-01"
-      subnet_id = module.snet_mgmt_management_uks.resource_id
+      name      = "nic-vm-dc-uks-01"
+      subnet_id = module.snet_dirservices_uks.resource_id
     }
   }
 
@@ -44,11 +48,5 @@ resource "random_password" "vm_admin" {
     storage_account_type = "Premium_LRS"
     disk_size_gb         = 128
   }
-
-resource "random_password" "vm_admin" {
-  length  = 20
-  special = true
-
-
 }
 
