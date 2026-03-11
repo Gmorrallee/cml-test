433484af88930bfeb96594d01bfa40d2b1e4b94e
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 12:59:12 +0000

    Create main_routetables_management.tf

---
+++ main_routetables_management.tf
@@ -0,0 +1,27 @@
+module "rt-mgmt-management-uks" {
+  source  = "Azure/avm-res-network-routetable/azurerm"
+  version = "0.5.0"
+
+
+  providers = {
+    azurerm = azurerm.management
+  }
+
+location = var.location
+name = "rt-mgmt-management-uks"
+  resource_group_name = azurerm_resource_group.rg_management_networking.name
+}
+
+module "rt-mgmt-management-ukw" {
+  source  = "Azure/avm-res-network-routetable/azurerm//examples/complete-example"
+  version = "0.5.0"
+
+
+  providers = {
+    azurerm = azurerm.management
+  }
+
+location = var.location_2
+name = "rt-mgmt-management-ukw"
+  resource_group_name = azurerm_resource_group.rg_management_networking.name
+}
