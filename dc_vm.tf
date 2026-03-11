297ef5f6bc0e628d392130d716eeb58dd4903f90
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 15:50:20 +0000

    Update dc_vm.tf

--- dc_vm.tf
+++ dc_vm.tf
@@ -1,15 +1,7 @@
-############################
-# Generate admin password
-############################
-
 resource "random_password" "vm_admin" {
   length  = 20
   special = true
 }
-
-############################
-# Domain Controller VM (UKS)
-############################
 
 module "vm_dc_uks" {
   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
@@ -19,16 +11,32 @@ module "vm_dc_uks" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg_identity_dirservices.name
 
-  os_type = "Windows"
-  size    = "Standard_D2s_v5"
+  # Required in v0.20+:
+  # If region has AZs, pick "1"/"2"/"3". If region has no AZs, set null.
+  zone = "1"
 
-  admin_username = "localadmin"
-  admin_password = random_password.vm_admin.result
+  # Credentials are supplied via the v0.20 account_credentials interface
+  account_credentials = {
+    admin_credentials = {
+      username = "localadmin"
+      password = random_password.vm_admin.result
+      # generate_admin_password_or_ssh_key defaults true, but we're providing password explicitly.
+    }
+  }
 
   network_interfaces = {
     primary = {
-      name      = "nic-vm-dc-uks-01"
-      subnet_id = module.snet_dirservices_uks.resource_id
+      name = "nic-vm-dc-uks-01"
+
+      ip_configurations = {
+        primary = {
+          name                        = "ipconfig1"
+          private_ip_subnet_resource_id = module.snet_mgmt_management_uks.resource_id
+          # optional:
+          # private_ip_address_allocation = "Dynamic"
+          # is_primary_ipconfiguration    = true
+        }
+      }
     }
   }
 
@@ -49,4 +57,5 @@ module "vm_dc_uks" {
     disk_size_gb         = 128
   }
 }
+``
 
