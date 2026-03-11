93141a4493d2c4ac46fa315ad74ad38ea28a6bcd
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 15:40:32 +0000

    Update dc_vm.tf

--- draft/dc_vm.tf
+++ draft/dc_vm.tf
@@ -1,1 +1,42 @@
 
+resource "azurerm_resource_group" "rg_management_management" {
+  provider = azurerm.management
+  name     = "rg-management-management"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_management_networking" {
+  provider = azurerm.management
+  name     = "rg-management-networking"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_connectivity_management" {
+  provider = azurerm.connectivity
+  name     = "rg-connectivity-management"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_connectivity_networking" {
+  provider = azurerm.connectivity
+  name     = "rg-connectivity-networking"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_identity_management" {
+  provider = azurerm.identity
+  name     = "rg-identity-management"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_identity_networking" {
+  provider = azurerm.identity
+  name     = "rg-identity-networking"
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg_identity_dirservices" {
+  provider = azurerm.identity
+  name     = "rg-identity-dirservices"
+  location = var.location
+}
