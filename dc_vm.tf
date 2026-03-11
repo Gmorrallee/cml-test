edc36168e665feb25eee612c5498f7424c14d299
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 15:58:33 +0000

    Update dc_vm.tf

--- dc_vm.tf
+++ dc_vm.tf
@@ -1,40 +1,29 @@
-resource "random_password" "vm_admin" {
-  length  = 20
-  special = true
-}
-
 module "vm_dc_uks" {
   source  = "Azure/avm-res-compute-virtualmachine/azurerm"
   version = "~> 0.20"
+  providers = { azurerm = azurerm }
 
   name                = "vm-dc-uks-01"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg_identity_dirservices.name
 
-  # Required in v0.20+:
-  # If region has AZs, pick "1"/"2"/"3". If region has no AZs, set null.
-  zone = "1"
+  zone = "1" # required; set null if you’re in a region without zones [3](https://registry.terraform.io/modules/Azure/avm-res-compute-virtualmachine/azurerm/latest)[4](https://github.com/Azure/terraform-azurerm-avm-res-compute-virtualmachine/blob/main/variables.tf)
 
-  # Credentials are supplied via the v0.20 account_credentials interface
   account_credentials = {
     admin_credentials = {
-      username = "localadmin"
-      password = random_password.vm_admin.result
-      # generate_admin_password_or_ssh_key defaults true, but we're providing password explicitly.
+      username                           = "localadmin"
+      password                           = random_password.vm_admin.result
+      generate_admin_password_or_ssh_key = false
     }
   }
 
   network_interfaces = {
     primary = {
       name = "nic-vm-dc-uks-01"
-
       ip_configurations = {
         primary = {
-          name                        = "ipconfig1"
-          private_ip_subnet_resource_id = module.snet_mgmt_management_uks.resource_id
-          # optional:
-          # private_ip_address_allocation = "Dynamic"
-          # is_primary_ipconfiguration    = true
+          name                          = "ipconfig1"
+          private_ip_subnet_resource_id = module.snet_idy_dirservices_uks.resource_id
         }
       }
     }
@@ -47,13 +36,5 @@ module "vm_dc_uks" {
     version   = "latest"
   }
 
-  managed_identities = {
-    system_assigned = true
-  }
-
-  os_disk = {
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-    disk_size_gb         = 128
-  }
+  managed_identities = { system_assigned = true }
 }
