a91981ee4c0e393682720e600a61f0cbac8bdb0d
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 15:42:51 +0000

    Update dc_vm.tf

--- draft/dc_vm.tf
+++ draft/dc_vm.tf
@@ -1,42 +1,54 @@
+module "vm_dc_uks" {
+  source  = "Azure/avm-res-compute-virtualmachine/azurerm"
+  version = "~> 0.20"
 
-resource "azurerm_resource_group" "rg_management_management" {
-  provider = azurerm.management
-  name     = "rg-management-management"
-  location = var.location
+  name                = "vm-dc-uks-01"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_identity_dirservices.name
+
+  os_type = "Windows"
+
+  size = "Standard_D2s_v5"
+
+resource "random_password" "vm_admin" {
+  length  = 20
+  special = true
+
+resource "random_password" "vm_admin" {
+  length  = 20
+  special = true
+
+  admin_username = "localadmin"
+  admin_password = admin_password = random_password.vm_admin.result
+
+  network_interfaces = {
+    primary = {
+      name      = "nic-vm-mgmt-uks-01"
+      subnet_id = module.snet_mgmt_management_uks.resource_id
+    }
+  }
+
+  source_image_reference = {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+  managed_identities = {
+    system_assigned = true
+  }
+
+  os_disk = {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    disk_size_gb         = 128
+  }
+
+resource "random_password" "vm_admin" {
+  length  = 20
+  special = true
+
+
 }
 
-resource "azurerm_resource_group" "rg_management_networking" {
-  provider = azurerm.management
-  name     = "rg-management-networking"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_connectivity_management" {
-  provider = azurerm.connectivity
-  name     = "rg-connectivity-management"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_connectivity_networking" {
-  provider = azurerm.connectivity
-  name     = "rg-connectivity-networking"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_identity_management" {
-  provider = azurerm.identity
-  name     = "rg-identity-management"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_identity_networking" {
-  provider = azurerm.identity
-  name     = "rg-identity-networking"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_identity_dirservices" {
-  provider = azurerm.identity
-  name     = "rg-identity-dirservices"
-  location = var.location
-}
