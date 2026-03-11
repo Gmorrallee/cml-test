6061e1ee2ad7cc1c6426c62a43b3e88a3d31c50a
Gmorrallee <100926882+Gmorrallee@users.noreply.github.com>
2026-03-11 09:57:59 +0000

    Update main_subnets_connectivity.tf

--- main_subnets_connectivity.tf
+++ main_subnets_connectivity.tf
@@ -8,6 +8,26 @@ module "snet-bastion-uks" {
   address_prefixes = ["10.102.250.0/24"]
 }
 
+module "snet-gateway-uks" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_connectivity_uksouth.resource_id
+  name      = "GatewaySubnet"
+
+  address_prefixes = ["10.102.251.0/24"]
+}
+
+module "snet-firewall-uks" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_connectivity_uksouth.resource_id
+  name      = "GatewaySubnet"
+
+  address_prefixes = ["10.102.252.0/24"]
+}
+
 module "snet-bastion-ukW" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
   version = "~> 0.17"
@@ -16,4 +36,24 @@ module "snet-bastion-ukW" {
   name      = "AzureBastionSubnet"
 
   address_prefixes = ["10.202.250.0/24"]
+}
+
+module "snet-gateway-ukw" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_connectivity_ukwest.resource_id
+  name      = "GatewaySubnet"
+
+  address_prefixes = ["10.102.251.0/24"]
+}
+
+module "snet-firewall-ukw" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm//modules/subnet"
+  version = "~> 0.17"
+
+  parent_id = module.vnet_connectivity_ukwest.resource_id
+  name      = "GatewaySubnet"
+
+  address_prefixes = ["10.202.252.0/24"]
 }
